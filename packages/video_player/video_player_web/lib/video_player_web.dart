88e6b06d1ae636c9c5b6c8599000b4b6382a68d0
Przemyslaw Tuzinek <przemyslaw.tuzinek@stepwise.pl>
2021-04-21 13:25:05 +0200

    changed controls and isInitialized to true

--- packages/video_player/video_player_web/lib/video_player_web.dart
+++ packages/video_player/video_player_web/lib/video_player_web.dart
@@ -163,7 +163,7 @@ class _VideoPlayer {
   final String uri;
   final int textureId;
   late VideoElement videoElement;
-  bool isInitialized = false;
+  bool isInitialized = true;
   bool isBuffering = false;
 
   void setBuffering(bool buffering) {
@@ -180,7 +180,7 @@ class _VideoPlayer {
     videoElement = VideoElement()
       ..src = uri
       ..autoplay = true
-      ..controls = false
+      ..controls = true
       ..style.border = 'none';
 
     // Allows Safari iOS to play the video inline
