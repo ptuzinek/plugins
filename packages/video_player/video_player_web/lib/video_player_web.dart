b41646ef29fc941d0291d252eaa72a08b746146f
Przemyslaw Tuzinek <przemyslaw.tuzinek@stepwise.pl>
2021-04-21 13:35:36 +0200

    changed isInitialized to false

--- packages/video_player/video_player_web/lib/video_player_web.dart
+++ packages/video_player/video_player_web/lib/video_player_web.dart
@@ -163,7 +163,7 @@ class _VideoPlayer {
   final String uri;
   final int textureId;
   late VideoElement videoElement;
-  bool isInitialized = true;
+  bool isInitialized = false;
   bool isBuffering = false;
 
   void setBuffering(bool buffering) {
